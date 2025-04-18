d51377614f137680e3633ff1539d38ee1551f499
Alastair Drong <adrong@gmail.com>
2025-04-18 11:56:35 -0700

    enable configuration from file

---
+++ constfig.py
@@ -0,0 +1,75 @@
+import yaml
+import logging
+
+from collections.abc import Iterable
+
+
+class _C(object):
+    def __init__(self):
+        # Default values for user configurable items
+        self.LISTEN_IP = "0.0.0.0"
+        self.LISTEN_PORT = 5000
+        self.API_KEYS = []
+
+        # Load user config (override defaults above)
+        self.load_config()
+
+    def set_constants(self):
+        # Constant values
+        pass
+
+    def load_config(self):
+        try:
+            with open("config.yaml", "r") as config_file:
+                config_string = config_file.read()
+
+            # Don't handle badly formatted YAML. Let the parser inform the user of the error.
+            configuration = yaml.load(config_string, Loader=yaml.SafeLoader)
+            if isinstance(configuration, dict):
+                for variable_name, value in configuration.items():
+                    setattr(self, variable_name, value)
+            else:
+                raise yaml.scanner.ScannerError(
+                    f"The file config.yaml should be structured as type dict, but got type {type(configuration)}"
+                )
+        except FileNotFoundError:
+            logging.warning(
+                "Configuration file config.yaml is missing. Using default values."
+            )
+        finally:
+            self.set_constants()
+            self.validate_config()  # Validate our config file
+
+    def _is_non_string_iterable(self, obj):
+        return isinstance(obj, Iterable) and not isinstance(obj, str)
+
+    def validate_config(self):
+        # Validate LISTEN_IP
+        assert isinstance(self.LISTEN_IP, str), "LISTEN_IP is not a string value"
+        assert (
+            len(self.LISTEN_IP.split(".")) == 4
+        ), "LISTEN_IP has an unexpected number of octets"
+        assert all(
+            [ip.isnumeric() for ip in self.LISTEN_IP.split(".")]
+        ), "LISTEN_IP is not a valid IP address."
+        assert all(
+            [0 <= int(ip) < 255 for ip in self.LISTEN_IP.split(".")]
+        ), "LISTEN_IP is not a valid IP address."
+
+        # Validate LISTEN_PORT
+        if isinstance(self.LISTEN_PORT, str):
+            assert self.LISTEN_PORT.isnumeric(), "LISTEN_PORT must be a whole number."
+            self.LISTEN_PORT = int(C.LISTEN_PORT)
+        assert 999 < self.LISTEN_PORT < 65536, "LISTEN_PORT is outside expected range."
+
+        # Validate API_KEYS
+        assert self._is_non_string_iterable(
+            self.API_KEYS
+        ), f"Expected list of API_KEYS, got {type(self.API_KEYS)}"
+        # A dictionary is technically valid, assuming they keys are the API keys, but for this reason, we can't include the type in the error message.
+        assert all(
+            [isinstance(key, str) for key in self.API_KEYS]
+        ), f"API_KEYS should be strings"
+
+
+C = _C()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from constfig import C
 
 pyxie = Flask(__name__)
 
